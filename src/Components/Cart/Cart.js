be8c224a9652dc4c1dfd686e529934ef7ef4384e
Francisco José Suárez Malatini <franciscosuarezmalatini@gmail.com>
2022-09-06 20:38:58 -0300

    nuevos cambios

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,16 @@
 import React, { useContext } from 'react';
 import { CartContext } from '../../Context/CartContext';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
-  const { cart } = useContext(CartContext);
+  const { cart, removeItem, emptyCart, totalCompra } = useContext(CartContext);
   if (cart.length === 0) {
-    return <h1>El carrito esta vacio</h1>;
+    return (
+      <>
+        <h1>El carrito esta vacio</h1>
+        <Link to="/">Volver al Inicio</Link>
+      </>
+    );
   }
   return (
     <>
@@ -12,9 +18,14 @@ const Cart = () => {
       {cart.map((product, key) => (
         <div key={key}>
           <div>{product.titulo}</div>
+          <p>Precio por unidad: ${product.precio}</p>
           <p>Cantidad: {product.qty}</p>
+          <button onClick={ () => {removeItem(product.id)}}>ELIMINAR</button>
         </div>
       ))}
+      <button onClick={emptyCart}>VACIAR CARRITO</button>
+      <p>PRECIO FINAL: ${totalCompra()}</p>
+      <button><Link to="/form">Finalizar compra</Link></button>
     </>
   );
 };
